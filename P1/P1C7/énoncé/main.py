8d5d369f436ce8631a89c36d0e2ace55c18c261a
Manase <attoessimanase@gmail.com>
2024-12-02 21:08:47 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+#Création du dictionnaire fruits
+fruits={"pomme":"rouge","banane":"jaune","orange":"orange"}
+
+#Ajout de la clé kiwi qui a pour valeur vert
+fruits['kiwi']="vert"
+
+couleur_banane=fruits['banane']
+
+#modification de la couleur de pomme
+fruits['pomme']="vert"
+
+#suppression de la clé banane du dictionnaire fruits
+del fruits("banane")
+
+#Affichage
+print(fruits)
