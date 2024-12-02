22d113164577e545241028831c9a7cb73c4112dd
Manase <attoessimanase@gmail.com>
2024-12-02 18:39:27 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+nom="Manase"
+age=19
+taille=1.80
+est_etudiant=True
+
+#Affichage des variables
+print(f"Nom: {nom}")
+print(f"Age= {age}")
+print(f"Taille= {taille}")
+print(f"Est étudiant :{est_etudiant}")
+
+#Affichage du type de chaque variable
+print(f"Type de Nom: {type(nom)}")
+print(f"Type de Age= {type(age)}")
+print(f"Type de Taille= {type(taille)}")
+print(f"Type de Est étudiant :{type(est_etudiant)}")
