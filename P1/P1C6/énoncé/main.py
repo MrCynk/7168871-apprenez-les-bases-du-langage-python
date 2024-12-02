15e99875e7693c72fd1319af47a71c61161b4714
Manase <attoessimanase@gmail.com>
2024-12-02 19:39:55 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
 # Écrivez votre code ici !
+fruits=["pomme","banane","orange"]
+
+#Ajout d'élément à la liste
+fruits.append("kiwi")
+
+#suppression d'élément de la liste
+fruits.remove("orange")
+
+#Modification du 2ème élément de la liste
+fruits[1]="ananas"
+
+#Affichage de la longueur de la liste
+print(f"La liste de fruits contient {len(fruits)} éléments")
+fruits.sort()
+print(fruits)
