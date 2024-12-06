8a0914b9df5abf23357599ad676cdd4c4d5b8200
Manase <attoessimanase@gmail.com>
2024-12-06 23:14:37 +0000

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,27 @@
-# Ecrivez votre code ici !
+#fourniture des 2 nombres
+nombre1=str(input("Entrez le 1er nombre : "))
+nombre2=str(input("Entrez le 2ème nombre : "))
+
+#vérification des nombres s'ils sont entier
+if nombre1.isnumeric() and nombre2.isnumeric():
+  nombre1=int(nombre1)
+  nombre2=int(nombre2)
+else:
+  raise SystemExit("Fin du programme")
+
+#creation de l'opération
+operation=input("Saisir l'opérateur souhaitéé : ")
+if (operation=="-"):
+  resultat=nombre1-nombre2
+elif (operation=="+"): 
+  resultat=nombre1+nombre2
+elif (operation=="*"):
+  resultat=nombre1*nombre2
+elif (operation=="/"):
+  if(nombre2 != 0):
+    resultat=nombre1/nombre2
+  else:
+    raise SystemExit("Impossible de diviser par 0")
+else :
+  raise SystemExit()
+print(nombre1,operation,nombre2,"=",round(resultat,2))
